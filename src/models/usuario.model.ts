b076e9feff155e6452c2afbefc13d8c4266e6bc2
Cleiber Salas <clsalab@gmail.com>
2024-03-10 23:38:55 -0500

    Creación de modelos

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,57 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Usuario extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  primerNombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  segundoNombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  primerApellido: string;
+
+  @property({
+    type: 'string',
+  })
+  segundoApellido?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  celular: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  clave: string;
+
+
+  constructor(data?: Partial<Usuario>) {
+    super(data);
+  }
+}
+
+export interface UsuarioRelations {
+  // describe navigational properties here
+}
+
+export type UsuarioWithRelations = Usuario & UsuarioRelations;
